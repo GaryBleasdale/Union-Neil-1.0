102e0beee169e42d11b347e2d88e919122ba7cbf
Gary Bleasdale <gary@bleasdale.dev>
2024-02-24 08:15:12 -0300

    added services section

--- global.js
+++ global.js
@@ -49,43 +49,8 @@ mobileMenu.addEventListener("click", (e) => {
 
 // Add event listeners to links inside the mobile menu to close the menu on click
 const menuLinks = mobileMenu.querySelectorAll("a");
-for (link of menuLinks) {
+for (const link of menuLinks) {
   link.addEventListener("click", () => {
     toggleMobileMenu();
-  });
-}
-
-// Google Map JS
-
-function initMap() {
-  const unionNeilOffice = { lat: -23.5122291, lng: -47.45891 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 16,
-    center: unionNeilOffice,
-  });
-  const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h3 id="firstHeading" class="firstHeading">Escritório Union Neil</h3>' +
-    '<div id="bodyContent">' +
-    "<p>Av. Washington Luiz 310, Torre White <br/> Jardim Faculdade, Sorocaba, SP <br/> 18031-000 <br/> <a href='https://goo.gl/maps/Rxa5RgpZ6ukGm8VQ7' target='_blank' style='text-decoration:underline;color:blue'>Abrir no Google Maps</a></p>" +
-    "</div>" +
-    "</div>";
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    ariaLabel: "Uluru",
-  });
-  const marker = new google.maps.marker.AdvancedMarkerElement({
-    position: unionNeilOffice,
-    icon: "img/union_neil_pin_80x106_2_65x86.png",
-    map: map,
-  });
-
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
-      map,
-    });
   });
 }
