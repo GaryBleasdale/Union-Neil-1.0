df28d0905b301524428cc896c3f8c910c53974da
Gary Bleasdale <gary@bleasdale.dev>
2024-04-05 19:23:14 -0300

    added new hero section, and moved video down

--- global.js
+++ global.js
@@ -6,14 +6,12 @@ window.addEventListener("scroll", () => {
 	if (window.scrollY > 1000) {
 		navbar.classList.add("fixed", "w-[100%]", "z-[1]");
 		navbar.classList.remove("relative");
-		whatsAppCTA.classList.remove("bg-[rgba(72,200,87,0.25)]");
-		whatsAppCTA.classList.add("bg-[rgba(72,200,87,0.15)]");
+		whatsAppCTA.classList.remove("hidden");
 		topNavbar.classList.add("hidden");
 	} else {
 		navbar.classList.remove("fixed");
 		navbar.classList.add("relative");
-		whatsAppCTA.classList.add("bg-[rgba(72,200,87,0.25)]");
-		whatsAppCTA.classList.remove("bg-[rgba(72,200,87,0.15)]");
+		whatsAppCTA.classList.add("hidden");
 		topNavbar.classList.remove("hidden");
 	}
 });
@@ -63,30 +61,30 @@ for (const link of menuLinks) {
 	});
 }
 
-// Wistia video API
-window._wq = window._wq || [];
-_wq.push({
-	id: "_all",
-	onReady: (video) => {
-		const initialModal = document.getElementById("initial-modal");
-		initialModal.classList.add("hidden");
-		const body = document.querySelector("body");
-		body.classList.remove("overflow-y-hidden");
+// // Wistia video API
+// window._wq = window._wq || [];
+// _wq.push({
+// 	id: "_all",
+// 	onReady: (video) => {
+// 		const initialModal = document.getElementById("initial-modal");
+// 		initialModal.classList.add("hidden");
+// 		const body = document.querySelector("body");
+// 		body.classList.remove("overflow-y-hidden");
 
-		video.bind("play", () => {
-			const initialModal = document.getElementById("initial-modal");
-			console.log("im", initialModal);
-			initialModal.classList.add("hidden");
-			console.log("Wistia video has started playing!");
-		});
+// 		video.bind("play", () => {
+// 			const initialModal = document.getElementById("initial-modal");
+// 			console.log("im", initialModal);
+// 			initialModal.classList.add("hidden");
+// 			console.log("Wistia video has started playing!");
+// 		});
 
-		// You can bind to other events in a similar manner
-		// video.bind("pause", function() {
-		//   console.log("Wistia video is paused.");
-		// });
+// 		// You can bind to other events in a similar manner
+// 		// video.bind("pause", function() {
+// 		//   console.log("Wistia video is paused.");
+// 		// });
 
-		// video.bind("end", function() {
-		//   console.log("Wistia video has ended.");
-		// });
-	},
-});
+// 		// video.bind("end", function() {
+// 		//   console.log("Wistia video has ended.");
+// 		// });
+// 	},
+// });
